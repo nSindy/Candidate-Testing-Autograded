68a74970169e59308a9cf04208be45e19e05a6f2
Nathan Sindelar <nathansindelar@macbook-pro-3.lan>
2024-06-20 16:23:27 -0500

    third try

--- candidate-testing.js
+++ candidate-testing.js
@@ -18,13 +18,13 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-let candidateName = input.question("Enter your first name. ");
+let candidateName = input.question("Enter your first name.");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 console.log(question);
-let candidateAnswer = input.question("Enter your response. ");
+let candidateAnswer = input.question("Enter your response.");
 
 }
 
