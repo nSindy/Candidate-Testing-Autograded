d64a49ada143394338d4eb6bcdbf469bfc44c938
Nathan Sindelar <nathansindelar@macbook-pro-3.lan>
2024-06-26 19:16:35 -0500

    Omg I think I'm finally there.

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,33 +11,42 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
-
-
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
+let correctAnswers = [
+	"Sally Ride",
+  "true",
+  "40",
+  "Trajectory",
+  "3"
+];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("Enter your first name. ");
 }
 
-function askQuestion() {
+function askQuestion() { 
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log(question);
-candidateAnswer = input.question("Enter your response. ");
-
+for (let i = 0; i < questions.length; i++){
+  answers = input.question(questions[i]);
+  candidateAnswers.push(answers);
+  }
+  
+  return candidateAnswers;
 }
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer){
-  console.log("Great job! Sally Ride was indeed the first American woman in space.");
-} else {
-  console.log("Incorrect.");
-}
+console.log(`You answered: ${candidateAnswers.join(', ')}. The correct answers are: ${correctAnswers.join(', ')}.`);
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
